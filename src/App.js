173aacc97e036c1b78646ecc5bbc5ab9736e3488
Sophie Newell <sophienewellstudio@gmail.com>
2023-02-06 14:10:48 -0600

    searches for content from today

--- src/App.js
+++ src/App.js
@@ -5,15 +5,26 @@ import searchContent from "./api";
 function App() {
   const [headline1, setHeadline1] = useState([]);
   const [line1, setLine1] = useState("");
+  const [line2, setLine2] = useState("");
+
+  const today = new Date();
+  const dateFormat = (date) => {
+    return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+  };
 
   const handleSubmit = async (term) => {
-    const result = await searchContent("castle");
-    setHeadline1(result[0].webTitle);
-    console.log(result);
+    const result = await searchContent("", dateFormat(today));
+    const ranNum = Math.floor(Math.random() * 10);
+    setHeadline1(result[ranNum].webTitle);
+    console.log(result, ranNum);
   };
   const handleShorten = () => {
-    const period = headline1.indexOf(":");
-    setLine1(headline1.slice(0, period));
+    headline1.includes(":")
+      ? //const colon = headline1.indexOf(":");
+        setLine1(headline1.slice(0, headline1.indexOf(":")))
+      : headline1.includes(",")
+      ? setLine1(headline1.slice(0, headline1.indexOf(",")))
+      : setLine1(headline1);
   };
 
   return (
@@ -21,7 +32,9 @@ function App() {
       <button onClick={handleSubmit}>Button</button>
       <div>{headline1}</div>
       <button onClick={handleShorten}>Shortened</button>
-      <div>{line1}</div>
+      <div>Line 1: {line1},</div>
+      <div>Line 2: {line2}</div>
+      <div>{dateFormat(today)}</div>
     </>
   );
 }
